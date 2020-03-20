0f0f9fe5fc71513743700630cff251d79b6d202b
Amasuo <oussamazay.1@gmail.com>
2020-03-20 16:24:55 +0100

    home template

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController{
+  /**
+   * @Route("/", name="home")
+   */
+   public function home():Response{
+     return $this->render('base.html.twig');
+   }
+}
