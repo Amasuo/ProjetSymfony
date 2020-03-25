a91c0a19c540a6c71fd97364c3e2384a0fee24de
SouhaMayara <mayara.souha@gmail.com>
2020-03-25 19:00:34 +0100

    profil

--- src/Controller/DoctorController.php
+++ src/Controller/DoctorController.php
@@ -66,4 +66,14 @@ class DoctorController extends AbstractController
         ]);
     }
 
+  /**
+     * @Route("/finddoctor/{id}", name="doctor")
+     */
+    public function findd($id )
+    {
+        $j=$this->getDoctrine()->getRepository(doctor::class)->find($id);
+        return $this->render('security/profil.html.twig', ['doctor' =>$j,
+        ]);   
+    }
+
 }
